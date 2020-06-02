a16b9a2e867fe7b9cf22683b333cad4ff71d9f4f
edmundo papi <edmundopierre@yahoo.com>
2020-06-02 03:15:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,10 @@
 def introduction_with_language (name)
-  puts "My first name is #{first_name}"
+  puts "My first name is #{first_name}."
 end
 
 
 def introduction_with_language (name,language)
-  puts "Hi, my name is  #{name} and I am to program in  #{language}"
+  puts "Hi, my name is  #{name} and I am to program in  #{language}."
 end
 
   
