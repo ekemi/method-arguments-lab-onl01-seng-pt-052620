c04b8629b1e90b1da831670bc2d1bb4c339c4cfa
edmundo papi <edmundopierre@yahoo.com>
2020-06-02 03:13:08 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def name (first_name)
+  puts "My first name is #{first_name}"
+end
+
+
+def name (first_name, last_name)
+  puts "My first name is #{first_name} and my last name is #{last_name}"
+end
+  
+  
